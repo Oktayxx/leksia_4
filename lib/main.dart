165a209a1b8888af9f56e91253f56d163ddf119d
Oktayxx <157093990+Oktayxx@users.noreply.github.com>
2024-01-26 14:55:30 +0600

    fist

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -14,11 +15,113 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar:AppBar(backgroundColor: Colors.green,),
-      body: Container(color: Colors.purple, height: 200, width: 100,
-      child: Text ("jjjj")),)
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text(
+              "MYAPP",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.red,
+                    primary: Colors.red,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Button",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Button"),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Button"),
+                ),
+                SizedBox(height: 20),
+                SizedBox(height: 5),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add_to_drive_rounded),
+                  label: Text("Data"),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.black,
+                        primary: Colors.blue,
+                        fixedSize: Size(100, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 5,
+                      ),
+                      onPressed: () {},
+                      child:
+                          Text("Button", style: TextStyle(color: Colors.white)),
+                    ),
+                    SizedBox(width: 60),
+                    Text(
+                      "Hello world!\nHello world!",
+                      textAlign: TextAlign
+                          .center,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Hello World! This is a Text Widget!",
+                  style: TextStyle(
+                    color: Colors.purple,
+                    backgroundColor: Colors.yellowAccent,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 8,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.speaker, size: 60, color: Colors.black),
+                    SizedBox(width: 10),
+                    Text("Speaker", style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
- }
+  }
 }
-
-
